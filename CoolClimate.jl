a0806d6851d951cc18fd9deaef11f76b9da59679
Garrett Kinman <garrettkinman@gmail.com>
2021-06-29 18:18:21 -0400

    first plot all nice!

--- CoolClimate.jl
+++ CoolClimate.jl
@@ -15,4 +15,8 @@ county_data = DataFrame(CSV.File("County-Results.csv"))
 ## explore simple data
 
 process_int(x::AbstractString) = @pipe x |> strip |> replace(_, ","=>"") |> parse(Int, _)
-scatter(process_int.(zipcode_data[:," popden "]), zipcode_data[:," Total Household Carbon Footprint (tCO2e/yr) "])
+scatter(process_int.(zipcode_data[:," popden "]), zipcode_data[:," Total Household Carbon Footprint (tCO2e/yr) "], legend=false)
+title!("Household Emissions vs Population Density by Zip Code")
+ylabel!("Total Household Carbon Footprint (tCO2e/yr)")
+xlabel!("Population Density (persons/sq mi)")
+
