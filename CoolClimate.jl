a6809b6955f1735e926a4e4943ff572f10136aaf
Garrett Kinman <garrettkinman@gmail.com>
2021-06-28 23:32:07 -0400

    loaded in all data

--- CoolClimate.jl
+++ CoolClimate.jl
@@ -1,6 +1,14 @@
+## imports
+
 using Plots
 using DataFrames
 using CSV
 using Flux
 
+## load in data
+
 zipcode_data = DataFrame(CSV.File("Zip-Code-Results.csv"))
+city_data = DataFrame(CSV.File("City-Results.csv"))
+county_data = DataFrame(CSV.File("County-Results.csv"))
+
+## explore simple data
